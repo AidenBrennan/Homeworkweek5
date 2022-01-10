01f51f7d6994f76f7ffbd6a7c57b3987d8ddcacc
Aiden Brennan <Aiden.c.b@hotmail.com>
2022-01-10 22:35:09 +1100

    Update script.js
    updated script to reflect changes to HTML

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 $(document).ready(function(){ 
-    $('.time-block').each(function(){
+    $('.row').each(function(){
 currenttimeblock = parseInt($(this).attr("id"));
 var meeting = localStorage.getItem(currenttimeblock);
 if (meeting) {
-$(this).children(".description").val(localStorage.getItem(currenttimeblock));
+$(this).children(".time-block").val(localStorage.getItem(currenttimeblock));
     }
 
 $(".saveBtn").on("click", function(){
-        var meeting = $(this).siblings(".description").val();
+        var meeting = $(this).siblings(".time-block").val();
         var time = $(this).parent().attr("id");
         localStorage.setItem(time, meeting);
 });
 
 $(".removeBtn").on("click", function() {
-        var meeting = $(this).siblings(".description").val();
+        var meeting = $(this).siblings(".time-block").val();
         var time = $(this).parent().attr("id");
         localStorage.removeItem(time, meeting);
         $(this).siblings('textarea').val('');
@@ -21,7 +21,7 @@ $(".removeBtn").on("click", function() {
 
 
 function currenttime() {
-    $('.time-block').each(function(){
+    $('.row').each(function(){
     $("#currentDay").text(moment().format("dddd, MMMM Do"));
     $("#currenttime").text(moment().format("HH:mm:ss"));
     var currenttimeblock =  parseInt($(this).attr("id"));
@@ -39,4 +39,3 @@ function currenttime() {
  setInterval(currenttime,1000);
     })
 })
-
